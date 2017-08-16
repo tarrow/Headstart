b7ee2fb7875a0425c1bdfa1bac22a9bb543099ff
Rainer Bachleitner <r.bachleitner@gmail.com>
2017-08-16 10:49:49 +0200

    First adaptation of csv to see if it works

---
+++ examples/openup_toolbox/data-config.js
@@ -0,0 +1,22 @@
+var data_config = {
+    tag: "visualization",
+    mode: "local_files",
+
+    title: "OpenUp Dissemination Toolbox",
+    input_format: "csv",
+    base_unit: "readers",
+    use_area_uri: false,
+    is_force_areas: false,
+    url_prefix: "http://mendeley.com/catalog/",
+    
+    show_timeline: false,
+    show_dropdown: false,
+    show_intro: false,
+    show_list:true,
+    is_force_papers:true,
+
+    files: [{
+        title: "toolbox",
+        file: "./data/toolbox.csv"
+    }]
+};
